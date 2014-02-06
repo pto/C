6088bba4ea1d9d43840f6d4b2361ddb6b9b9129f
Peter Olsen <pto@me.com>
2014-02-05 19:08:14 -0500

    final temps

--- ch01/temps.c
+++ ch01/temps.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
-// Print Fahrenheit-Celsius table for 0, 20, ..., 300 degrees F
+// Print Fahrenheit-Celsius table
+
+const int lower = 0;	// Range for temperatures in degress F
+const int upper = 300;
+const int step = 20;
 
 int main(void)
 {
-	int lower = 0;
-	int upper = 300;
-	int step = 20;
-
-	float fahr = lower;
-	while (fahr <= upper) {
+	for (int fahr = lower; fahr <= upper; fahr += step) {
 		float celsius = 5.0 / 9.0 * (fahr - 32.0);
-		printf("%3.0f %6.1f\n", fahr, celsius);
-		fahr = fahr + step;
+		printf("%3d %6.1f\n", fahr, celsius);
 	}
+
 	return 0;
 }
