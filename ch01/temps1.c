9aa55396207a54f17b78e0d921aa94c91d0b6dd9
Peter Olsen <pto@me.com>
2014-06-01 15:01:56 -0400

    strict conformance to the canonical text

--- ch01/temps1.c
+++ ch01/temps1.c
@@ -7,10 +7,15 @@ main()
 {
 	int fahr, celsius;
 	int lower, upper, step;
-	for (int fahr = lower; fahr <= upper; fahr += step) {
-		float celsius = 5.0 / 9.0 * (fahr - 32.0);
-		printf("%3d %6.1f\n", fahr, celsius);
-	}
 
-	return 0;
+	lower = 0;		/* lower limit of temperature table */
+	upper = 300;	/* upper limit */
+	step = 20;		/* step size */
+
+	fahr = lower;
+	while (fahr <= upper) {
+		celsius = 5 * (fahr - 32) / 9;
+		printf("%d\t%d\n", fahr, celsius);
+		fahr = fahr + step;
+	}
 }
