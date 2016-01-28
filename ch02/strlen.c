4824a79f61b1c37fb348dbb94ada46b02154f583
Peter Olsen <pto@me.com>
2016-01-27 19:15:52 -0500

    imitation strlen

---
+++ ch02/strlen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int strlen(char s[]);
+
+main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		printf("usage: strlen <string>\n");
+		return 1;
+	}
+
+	printf("%d\n", strlen(argv[1]));
+
+	return 0;
+}
+
+/* strlen:  return length of s */
+int strlen(char s[])
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		++i;
+	return i;
+}
