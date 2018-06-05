020d45600cede8cff2b03cc5bef3e295739579ae
Peter Olsen <pto@me.com>
2018-06-04 20:35:53 -0400

    cleanup and entabbing

--- ch04/calc.c
+++ ch04/calc.c
@@ -5,12 +5,12 @@
 /* rudimentary calculator */
 main()
 {
-    double sum, atof(char []);
-    char line[MAXLINE];
-    int getline(char line[], int max);
+	double sum, atof(char []);
+	char line[MAXLINE];
+	int getline(char line[], int max);
 
-    sum = 0;
-    while (getline(line, MAXLINE) > 0)
-        printf("\t%g\n", sum += atof(line));
-    return 0;
+	sum = 0;
+	while (getline(line, MAXLINE) > 0)
+		printf("\t%g\n", sum += atof(line));
+	return 0;
 }
