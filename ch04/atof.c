bc5353889db07d575fcf823688ed21a151a5eae1
Peter Olsen <pto@me.com>
2018-06-04 20:16:39 -0400

    string to double

---
+++ ch04/atof.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <ctype.h>
+
+double atof(char s[]);
+
+main(int argc, char *argv[])
+{
+	if (argc != 2) {
+		fprintf(stderr, "usage: atof <string to convert>\n");
+		return 1;
+	}
+
+	printf("%.15g\n", atof(argv[1]));
+
+	return 0;
+}
+
+/* atof:  convert s to double */
+double atof(char s[])
+{
+    double val, power;
+    int i, sign;
+
+    for (i = 0; isspace(s[i]); i++)  /* skip white space */
+        ;
+    sign = (s[i] == '-') ? -1 : 1;
+    if (s[i] == '+' || s[i] == '-')
+        i++;
+    for (val = 0.0; isdigit(s[i]); i++)
+        val = 10.0 * val + (s[i] - '0');
+    if (s[i] == '.')
+        i++;
+    for (power = 1.0; isdigit(s[i]); i++) {
+        val = 10.0 * val + (s[i] - '0');
+        power *= 10.0;
+    }
+    return sign * val / power;
+}
